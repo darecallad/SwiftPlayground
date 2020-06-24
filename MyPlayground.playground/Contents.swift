3af7e905545709f3599eaf9d845f8e24b13d950c
Chung-An Chen <chung-anchen@Chung-Ande-MacBook-Air.local>
2020-06-24 15:18:50 -0700

    first

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,43 @@
+import UIKit
+
+// Variables use var, it can be String, int..etc.
+
+var name = "John"
+var age = 15
+var length = 15.2
+var isTruth = false
+
+// change variables after you declare
+
+age = 16
+name = "Johnson"
+
+// --------------------------------------------
+
+// multiple line string
+
+var str = """
+this is multiple string lane1 \
+and this for second lane
+"""
+//
+
+// String interpolation
+
+var str1 = "Jonny"
+var score = 99
+var result = "\(str1) got  \(score) in Math test"
+
+// Constant  use let and cannot be changed
+
+let Jmoney = 10000
+Jmoney
+//
+// you can declare varible or constant byurslef
+var age1: Int = 18
+var name1 : String = "Jona"
+let Fname : String = "Jojo"
+
+// first part 
+
+
